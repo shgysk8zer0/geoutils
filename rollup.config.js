b3ab98451ff1e2b7270d87f0077b98a2b36b0fc0
Chris Zuber <shgysk8zer0@gmail.com>
2024-12-28 12:15:21 -0800

    Bundle everything in `geoutils.cjs`

--- rollup.config.js
+++ rollup.config.js
@@ -1,28 +1,17 @@
 import terser from '@rollup/plugin-terser';
+import nodeResolve from '@rollup/plugin-node-resolve';
 
 const modules = ['consts', 'coords', 'hash', 'uri', 'utils'];
 const external = (id, input) => id !== input;
 
 export default [{
 	input: 'geoutils.js',
-	output: {
-		file: 'geoutils.min.js',
-		format: 'esm',
-		plugins: [terser()],
-		sourcemap: true,
-	},
-}, {
-	input: 'geoutils.js',
-	external,
-	output: {
-		file: 'geoutils.cjs',
-		format: 'cjs',
-	}
+	plugins: [nodeResolve()],
+	output: [
+		{ file: 'geoutils.min.js', format: 'esm', plugins: [terser()], sourcemap: true },
+		{ file: 'geoutils.cjs', format: 'cjs' }],
 }, ...modules.map(path => ({
 	input: `${path}.js`,
 	external,
-	output: {
-		file: `${path}.cjs`,
-		format: 'cjs',
-	}
+	output: { file: `${path}.cjs`, format: 'cjs' }
 }))];
